dd9aa7caf6cce02ba8a3c614758781d57de2dd79
Vivianne Zsoldos <vdzsoldos@student.fullsail.edu>
2024-02-07 22:57:27 -0500

    Added some Frames :-)
    Minor

--- Assets/Scripts/UI/GameManager.cs
+++ Assets/Scripts/UI/GameManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
-{
+{ 
+    [SerializeField] RawImage frameChoice;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,16 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void DisplayGunImage(Texture2D texture)
+    {
+        //References to Textures that are added in the GunSO on Unity
+        {
+            if (texture != null)
+            {
+                frameChoice.texture = texture;
+            }
+        }
     }
 }
