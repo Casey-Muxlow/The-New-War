1fa4d44ba2b0e715f8e916fbb0818a77a531b119
Casey-Muxlow <muxlowcasey@gmail.com>
2024-03-03 18:26:31 -0500

    soldier and script updates

---
+++ Assets/Scripts/Weapon/GunScriptableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gun", menuName = "Guns/Gun", order = 0)]
+public class GunScriptableObject : ScriptableObject
+{
+    
+
+}
